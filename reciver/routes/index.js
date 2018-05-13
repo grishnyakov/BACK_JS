7c1344737590284facea8d8452fb470aa5e88066
Sergio <grishnyakov2@ya.ru>
2018-05-13 19:29:21 +0500

    multyply params

--- reciver/routes/index.js
+++ reciver/routes/index.js
@@ -33,13 +33,49 @@ function handleDisconnect() {
 }
 handleDisconnect();
 
-function splitArrayStr(arrStr) {
+function splitAndCheckArrayStr(arrStr,db_params) {
     if(arrStr){
+        console.log("db_params",db_params);
         let arrayParamsTemp = arrStr.split('&'); // strings params  item as   id=123412
-        let Params = []; // objects  item as  {key: id, value: 123412}
+        let Params = {}; // objects  item as  {key: id, {value: 123412, id_danger: 0}
         for(let index in arrayParamsTemp) {
             if(arrayParamsTemp[index].indexOf('=') > -1) {
-                Params[arrayParamsTemp[index].split('=')[0]] = arrayParamsTemp[index].split('=')[1];
+                let short_name = arrayParamsTemp[index].split('=')[0];
+                let value = arrayParamsTemp[index].split('=')[1];
+                let id_danger = 0;
+                //step 2 (check parametr on alert)
+                for(let key in db_params) {
+                    console.log("db_params[key]",db_params[key]);
+
+                    if(db_params[key].short_name === short_name) {
+
+                        // булевые 0 - проблема
+                        // булевые 1 - ок
+
+
+                        if(db_params[key].id_type_parametr == 1) {  //id_type_parametr = 0 - bool 1- diapason
+
+                            if(db_params[key].danger_value_min >= value || db_params[key].danger_value_max <= value)
+                                id_danger = 2; // 2 - AVARIYA
+                            else id_danger = 1; // 1 - OK
+                        }
+                        else {
+                            if(value == 0)
+                                id_danger = 2; // 2 - AVARIYA
+                            else id_danger = 1; // 1 - OK
+                        }
+
+                        break;
+                    }
+                }
+
+
+                //push in array
+                Params[short_name] = {
+                        value: value,
+                        id_danger: id_danger
+                    };
+                console.log(Params[short_name]);
             }
             else console.error("splitArrayStr:: BAD PARAMETR:",arrayParamsTemp[index]," i continue...");
         }
@@ -55,9 +91,31 @@ app.get('/p/:params', function(req, res) {
 
     console.log(date," ip:",ip+" APP.GET::",req.params.params);
 
-    let Params = splitArrayStr(req.params.params);
+    /* pre-inserting actions
+        1. select * from parametrs (get max and min value)
+    */
 
-    insertQuery(res, Params);
+    let query = "SELECT * FROM parametrs;";
+    connection.query(query, function (err, result) {
+        if (!err && result) {
+            let db_params = result;
+            let id_danger_forgroup = 0;
+            let Params = splitAndCheckArrayStr(req.params.params,db_params);
+            for(let key in Params) {
+                if(Params[key].id_danger == 2) {
+                    id_danger_forgroup = 2;
+                    break;
+                }
+                else id_danger_forgroup = 1;
+            }
+            insertQuery(res, Params, id_danger_forgroup);
+        }
+        else {
+            console.error(err);
+        }
+    });
+
+
 });
 
 
@@ -66,7 +124,7 @@ let getValuesStr = function(Params,id_group){
     //(id_group,type_parametr,value)
     for(let key in Params) {
         if(key !== "id" && key !=="pas") {
-            str+="("+id_group+",'"+key+"',"+Params[key]+"),";
+            str+="("+id_group+",'"+key+"',"+Params[key].value+","+Params[key].id_danger+"),";
         }
     }
     if(str.length>0)
@@ -74,17 +132,18 @@ let getValuesStr = function(Params,id_group){
     return str;
 };
 
-let insertQuery = function (res, params) {
+let insertQuery = function (res, params,id_danger_forgroup) {
 
     if(params) {
         /*algoritm inserting values:
             1. check existion device
-            2. create group messages
-            3. create params into messages  with group_id (receive from step 2)
+            2. check params on alerts
+            3. create group messages( receive type_danger from step 2)
+            4. create params into messages with group_id (receive from step 3)
         */
 
         //step 1
-        let queryAuth = "SELECT * FROM devices WHERE id = " + params['id'] + ";";
+        let queryAuth = "SELECT * FROM devices WHERE id = " + params['id'].value + ";";
         connection.query(queryAuth, function (err, result) {
             if (err) {
                 console.error(err.sqlMessage);
@@ -92,12 +151,12 @@ let insertQuery = function (res, params) {
             }
             else {
                 if(result.length > 0) {
-                    if (result[0].password ===  params['pas']) {
+                    if (result[0].password ===  params['pas'].value) {
                         console.log("auth device success", result);
 
-                        //step 2
+                        //step 3
                         //INSERT INTO tbl_name (a,b,c) VALUES(1,2,3),(4,5,6),(7,8,9);
-                        let queryInsertGroup = "INSERT INTO groups_message (id_device) VALUES ("+params['id']+")";
+                        let queryInsertGroup = "INSERT INTO groups_message (id_device,id_danger) VALUES ("+params['id'].value+","+id_danger_forgroup+")";
 
                         connection.query(queryInsertGroup, function (err, result) {
                             if (err) {
@@ -105,13 +164,13 @@ let insertQuery = function (res, params) {
                                 res.sendStatus(500); //500 Internal Server Error («внутренняя ошибка сервера»)
                             }
                             else {
-                                //step 3
+                                //step 4
                                     if(result) {
 
                                         let id_group = result.insertId;
                                         console.log(" new id_group:", id_group);
                                         let values = getValuesStr(params, id_group);
-                                        let queryInsertValue = "INSERT INTO messages (id_group,type_parametr,value) VALUES " + values + ";";
+                                        let queryInsertValue = "INSERT INTO messages (id_group,type_parametr,value,id_danger) VALUES " + values + ";";
                                         connection.query(queryInsertValue, function (err, result) {
                                             console.log(queryInsertValue);
                                             if (err) {
